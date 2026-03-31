8366b1700733c89ee8b1a27c7b90885c06d134e2
abirlouhichi03 <louhichiabir@tbs.u-tunis.tn>
2026-03-31 16:03:55 +0100

    Week 5: Arrow functions and event handlers

--- react-lab1.2/src/App.jsx
+++ react-lab1.2/src/App.jsx
@@ -25,66 +25,55 @@ const stories = [
   num_comments: 60
   }
 ];
-function App() {
-
-  console.log(stories[0]);
-
+const App = () => {
   return (
     <div>
       <h1>Hacker News Stories</h1>
       <Header />
       <Search />
-      <List /> 
-       
+      <List />
     </div>
   );
-}
+};
 
 export default App;
 
-function List() {
-  return (
-    <div>
-      {stories.map((story) => {
-        return (
-          <div key={story.objectID}>
-            <h3>
-              <a href={story.url} target="_blank" rel="noreferrer">
-                {story.title}
-              </a>
-            </h3>
-            <p>Author: {story.author}</p>
-            <span>Points: {story.points} </span>
-            <span>Comments: {story.num_comments}</span>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
-function Search() {
+const List = () => (
+  <div>
+    {stories.map((story) => (
+      <div key={story.objectID}>
+        <p>{story.title}</p>
+      </div>
+    ))}
+  </div>
+);
+const Search = () => {
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    console.log("User is typing...");
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
     </div>
   );
-}
-function Header() {
+};
+const Header = () => {
   return <h1>My Hacker News App 🚀</h1>;
-}
+};
+
 /*
 Reflection:
 
-1. What does App do?
-It organizes and renders main components.
+1. Concise body:
+Used when returning one simple expression.
 
-2. What does List do?
-It displays the list of stories.
+2. Block body:
+Used when we need logic (variables, functions).
 
-3. What does Search do?
-It displays the search input.
-
-4. Why cleaner?
-Because each component has one responsibility.
+3. Event object:
+Contains information about the event (input value, target, etc).
 */
